0ec8f25858349f8570c720962dc61cf04c82a807
Ebenezr <ebenezarbukosia@gmail.com>
2022-10-08 11:37:54 +0300

    [ADD] add form modal template

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,27 @@ import "./sass/style.scss";
 import DoctorTBL from "./components/Tables/Doctortable";
 import NurseTBL from "./components/Tables/NurseTBL";
 import TriageTBL from "./components/Tables/TriageTBL";
+import VitalsModal from "./components/Modals/patientVitalsModal";
+import { motion } from "framer-motion";
+import { useState } from "react";
 function App() {
+  const [openModal, setOpenModal] = useState(false);
   return (
     <main>
       <Sidenav />
       <TriageTBL />
+      <motion.button
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        className="btn main-btn"
+        onClick={() => setOpenModal(true)}
+      >
+        Add new
+      </motion.button>
+      <VitalsModal
+        openModal={openModal}
+        closeModal={() => setOpenModal(false)}
+      />
     </main>
   );
 }
---
+++ src/components/Modals/patientVitalsModal.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { AiFillCloseSquare } from "react-icons/ai";
+import { motion } from "framer-motion";
+
+const dropIn = {
+  hidden: {
+    y: "-100vh",
+    opacity: 0,
+  },
+  visible: {
+    y: "0",
+    opacity: 1,
+    transition: {
+      duration: 0.1,
+      type: "spring",
+      damping: 25,
+      stiffness: 500,
+    },
+  },
+  exit: {
+    y: "100vh",
+    opacity: 0,
+  },
+};
+
+interface ModalProps {
+  openModal: boolean;
+  closeModal(): void;
+}
+
+const VitalsModal: React.FC<ModalProps> = ({ openModal, closeModal }) => {
+  if (!openModal) return null;
+  return (
+    <motion.div
+      className="overlay"
+      onClick={closeModal}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      <motion.section
+        variants={dropIn}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        className="modal"
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <header className="modal-header">
+          <h2>Modal form</h2>
+          <button>
+            <AiFillCloseSquare
+              className="modal-close-icon"
+              onClick={closeModal}
+            />
+          </button>
+        </header>
+        <article></article>
+        <footer className="modal-footer">
+          <button className="btn save">Action</button>
+          <button className="btn close" onClick={closeModal}>
+            Close
+          </button>
+        </footer>
+      </motion.section>
+    </motion.div>
+  );
+};
+
+export default VitalsModal;
